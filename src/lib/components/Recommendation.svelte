bd6b7711b67f0fa91e71c1f3f1766579f981c2d0
Haruaki <misbahulk2236@gmail.com>
2024-03-31 18:36:58 +0700

    feat: recommendation content

--- src/lib/components/Recommendation.svelte
+++ src/lib/components/Recommendation.svelte
@@ -1,1 +1,44 @@
-anjai
+<p>
+	Submission Anda akan dinilai oleh Reviewer guna mendapatkan sertifikat StudyJam Web Programming
+	Advanced. Untuk mendapat sertifikat ini, proyek MusicShelf API harus memenuhi seluruh pengujian
+	otomatis pada seluruh Postman Request. Bila salah satu pengujiannya gagal, maka submission Anda
+	akan kami tolak.
+</p>
+
+<p>
+	Submission Anda akan dinilai oleh Reviewer dengan skala 1-3. Untuk mendapatkan nilai tinggi, Anda
+	bisa menerapkan beberapa saran berikut:
+</p>
+<ul>
+	<li>Memenuhi kriteria opsional yang diberikan.</li>
+	<li>
+		Menuliskan kode dengan bersih:
+		<ul>
+			<li>Menghapus kode yang redundan (tidak perlu).</li>
+			<li>Menghapus berkas yang tidak digunakan.</li>
+		</ul>
+	</li>
+</ul>
+
+<p>Berikut adalah detail penilaian submission:</p>
+<ul>
+	<li>
+		<strong>Bintang 1</strong> : Seluruh kriteria wajib terpenuhi, namun terdapat kekurangan pada penulisan
+		kode (tidak rapi) atau gaya penulisan tidak konsisten.
+	</li>
+	<li>
+		<strong>Bintang 2</strong> : Seluruh kriteria wajib terpenuhi, namun tidak terdapat improvisasi atau
+		kriteria opsional yang dipenuhi.
+	</li>
+	<li>
+		<strong>Bintang 3</strong> : Seluruh kriteria wajib terpenuhi dan menerapkan minimal satu kriteria
+		opsional yang ada.
+	</li>
+</ul>
+
+<h2>Catatan</h2>
+
+<p>
+	Jika submission Anda ditolak maka tidak ada penilaian. Penilaian bintang di atas hanya berlaku
+	jika submission Anda lulus.
+</p>
