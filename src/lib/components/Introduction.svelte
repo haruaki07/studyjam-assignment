450be24c086da406c12b70ffdf7d261a8b3fd05c
Haruaki <misbahulk2236@gmail.com>
2024-04-01 17:19:35 +0700

    feat: introduction content

--- src/lib/components/Introduction.svelte
+++ src/lib/components/Introduction.svelte
@@ -1,1 +1,19 @@
-p
+<p>
+	Selamat! Akhirnya Anda telah sampai ke tahap Mini Project StudyJam Web Programming Advanced.
+	Selama ini, Anda telah mempelajari:
+</p>
+
+<ul>
+	<li>Dasar-dasar Object Oriented Programming (OOP) dalam PHP;</li>
+	<li>Konsep MVC dan membuat Program CRUD PHP sederhana yang menerapkan MVC;</li>
+	<li>Menggunakan database SQLite3 dan MySQL pada program PHP;</li>
+	<li>Pengenalan framework Laravel;</li>
+	<li>dan Membuat program API CRUD sederhana dengan menggunakan Laravel.</li>
+</ul>
+
+<p>Tentu Anda juga sudah mengerjakan seluruh latihan yang diberikan pada kelas ini.</p>
+<p>
+	Untuk bisa lulus dan mendapatkan sertifikat StudyJam Web Programming Advanced ini, Anda harus
+	mengerjakan tugas yakni membuat proyek <strong>MusicShelf API</strong> sesuai kriteria lengkap di bawah
+	ini. Tim Reviewer akan memeriksa pekerjaan Anda dan memberikan review pada proyek yang Anda buat.
+</p>
