7e1630e976845f27a776bdeed3cb2d1e7f9939e4
Haruaki <misbahulk2236@gmail.com>
2024-04-03 08:47:41 +0700

    fix: green color
    * fix: period

--- src/routes/logout/+page.server.ts
+++ src/routes/logout/+page.server.ts
@@ -3,9 +3,15 @@ import type { Actions } from './$types';
 import { setFlash } from 'sveltekit-flash-message/server';
 
 export const actions: Actions = {
-    async default({ locals: { supabase }, cookies }) {
-        await supabase.auth.signOut();
-        setFlash({ type: "success", message: "Anda telah logout dari akun. Harap login kembali untuk melanjutkan" }, cookies);
-        redirect(303, '/');
-    }
+	async default({ locals: { supabase }, cookies }) {
+		await supabase.auth.signOut();
+		setFlash(
+			{
+				type: 'success',
+				message: 'Anda telah logout dari akun. Harap login kembali untuk melanjutkan.'
+			},
+			cookies
+		);
+		redirect(303, '/');
+	}
 };
